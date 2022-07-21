e07539d70b5e9e3ad41b513b9f2e02f2631cf556
Liedson Correia <liedsoncanal@gmail.com>
2022-07-21 03:54:28 -0100

    update preview

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
 import Head from 'next/head'
+import { getAllPosts } from "../lib/posts";
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
 
 export default function Home() {
+  const posts = getAllPosts();
   return (
-      <div>
+      <div className={styles.container}>
+        <Head>Cooking with Tuomo</Head>
         <div>
             <h1>Cooking w/ Tuomo</h1>
         </div>
-        <div>posts will go here</div>
-
-
-
+        <div>
+          {posts.map(p => <BlogPostPreview key={p.id} data={p}/>)}
+        </div>
 
       </div>
   )
+}
+
+
+
+const BlogPostPreview = (props) => {
+  const { data } = props;
+    return <div style={{ maxWidth: "400px" }}>
+        <Image 
+        src={data.coverImage} 
+        alt={data.title}
+        width= {1200}
+        height= {800} 
+        />
+        <h2>{data.title}</h2>
+        <div>{data.publishDate}</div>
+        <p>{data.excerpt}</p>
+          <div style={{fontWeight: 'bold'}}>{data.author}</div>
+
+    </div>;
 }
